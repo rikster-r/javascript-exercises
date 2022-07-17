01acd9f3acf0b2f8faae504e47126e11c54ca94a
rikster-r <seitdanial43@gmail.com>
2022-07-17 16:54:34 +0600

    Complete tasks 1 to 6

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
+    
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        output+=string;
+    }
+    return output;
 };
-
+s
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let returnArray = array;
+    for (let elem of args) {
+        returnArray = returnArray.filter(element => element !== elem);
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
